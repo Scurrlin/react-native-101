967e7c88a39630d0471dc34944db86bbfa8cc985
Sean Currlin <sean.currlin@gmail.com>
2024-05-18 17:32:48 -0400

    fix minor UI issues in index.jsx

--- app/index.jsx
+++ app/index.jsx
@@ -15,7 +15,7 @@ export default function App() {
   return (
     <SafeAreaView className="h-full bg-primary">
       <ScrollView contentContainerStyle={{ height: "100%" }}>
-        <View className="h-full  w-full items-center justify-center px-4">
+        <View className="h-full w-full items-center justify-center px-4">
           <Image
             source={images.logo}
             className="h-[84px] w-[130px]"
@@ -23,10 +23,11 @@ export default function App() {
           />
 
           <Image
-            className="h-[300px] w-full max-w-[380px]"
+            className="h-[298px] w-full max-w-[380px]"
             source={images.cards}
             resizeMode="contain"
           />
+
           <View className="relative mt-5">
             <Text className="text-center text-3xl font-bold text-white">
               Discover Endless{"\n"}
@@ -40,6 +41,7 @@ export default function App() {
               resizeMode="contain"
             />
           </View>
+
           <Text className="mt-7 text-center font-pregular text-sm text-gray-100">
             Where Creativity Meets Innovation: Embark on a Journey of Limitless
             Exploration with Aora
@@ -49,10 +51,12 @@ export default function App() {
             title="Continue with Email"
             handlePress={() => router.push("/sign-in")}
             containerStyles="w-full mt-7"
+            textStyle="font-bold"
           />
         </View>
       </ScrollView>
-      <StatusBar backgroundColor="#161622" style="light" />
+
+      <StatusBar style="light" backgroundColor="#161622" />
     </SafeAreaView>
   );
-}
+};
